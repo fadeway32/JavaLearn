6b7a7e85557880271ebd92ccdfba90f859708f5e
刘刚 <2cevening@gmail.com>
2022-06-18 09:33:20 +0800

    feature(elkDemo):first commit

--- elkDemo/src/main/java/com/learn/elk/EsTestClient.java
+++ elkDemo/src/main/java/com/learn/elk/EsTestClient.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class EsTestClient {
 
     public static void main(String[] args) throws IOException {
+        //all right
         RestHighLevelClient restHighLevelClient =new RestHighLevelClient(
                  RestClient.builder(new HttpHost("192.168.30.133",9200,"http"))
         );
